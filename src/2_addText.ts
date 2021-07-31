6aafd8b9902640878e1fe6cf0cb69de9c2542af4
LuccaHellriegel <lucca.hellriegel@gmx.net>
2021-08-01 00:13:41 +0200

    preparation for automatic mapping of settings to correct function

--- src/2_addText.ts
+++ src/2_addText.ts
@@ -70,3 +70,95 @@ export const appendToDailyNote = makeAddText(append, getDailyNoteFile);
 export const prependToDailyNote = makeAddText(prepend, getDailyNoteFile);
 export const appendToDailyNoteHeading = makeAddText(appendToHeading, getDailyNoteFile);
 export const prependToDailyNoteHeading = makeAddText(prependToHeading, getDailyNoteFile);
+
+export enum NoteOption {
+	NAMED = "named note",
+	DAILY = "daily note",
+}
+
+enum ModifyType {
+	SIMPLE = "simple",
+	HEADING = "heading",
+}
+
+type SimpleModifyType = AddText<InputOptions, PluginOptions>;
+
+enum SimpleModifyOption {
+	APPEND = "append",
+	PREPEND = "prepend",
+}
+
+type HeadingModifyType = AddText<HeadingOptions, PluginOptions>;
+
+enum HeadingModifyOption {
+	APPEND_TO_HEADING = "append to heading",
+	PREPEND_TO_HEADING = "prepend to heading",
+}
+
+export const ModifyOptions = Object.fromEntries(
+	[...Object.entries(SimpleModifyOption), ...Object.entries(HeadingModifyOption)].sort()
+);
+
+// reverse mapping here because the user does not need to know that these are separate
+// but we still need to map them to different functions!
+const modifyOptionToTypeMap = {
+	...Object.keys(SimpleModifyOption).reduce((p, c) => {
+		p[c] = ModifyType.SIMPLE;
+		return p;
+	}, {} as any),
+	...Object.keys(HeadingModifyOption).reduce((p, c) => {
+		p[c] = ModifyType.HEADING;
+		return p;
+	}, {} as any),
+};
+
+type SimplyModifyOptionMap = { [key in SimpleModifyOption]: SimpleModifyType };
+
+const dailySimpleModifyOptions: SimplyModifyOptionMap = {
+	[SimpleModifyOption.APPEND]: appendToDailyNote,
+	[SimpleModifyOption.PREPEND]: prependToDailyNote,
+};
+
+const noteSimpleModifyOptions: SimplyModifyOptionMap = {
+	[SimpleModifyOption.APPEND]: appendToNote,
+	[SimpleModifyOption.PREPEND]: prependToNote,
+};
+
+type HeadingModifyOptionMap = { [key in HeadingModifyOption]: HeadingModifyType };
+
+const dailyHeadingModifyOptions: HeadingModifyOptionMap = {
+	[HeadingModifyOption.APPEND_TO_HEADING]: appendToDailyNoteHeading,
+	[HeadingModifyOption.PREPEND_TO_HEADING]: prependToDailyNoteHeading,
+};
+
+const noteHeadingModifyOptions: HeadingModifyOptionMap = {
+	[HeadingModifyOption.APPEND_TO_HEADING]: appendToNoteHeading,
+	[HeadingModifyOption.PREPEND_TO_HEADING]: prependToNoteHeading,
+};
+
+export const optionsForAddText: {
+	noteOptions: (keyof typeof NoteOption)[];
+	modifyOptions: (keyof typeof SimpleModifyOption | keyof typeof HeadingModifyOption)[];
+	modifyOptionToTypeMap: { [key in SimpleModifyOption | HeadingModifyOption]: ModifyType };
+	functionMap: {
+		[ModifyType.SIMPLE]: { [key in NoteOption]: SimplyModifyOptionMap };
+		[ModifyType.HEADING]: { [key in NoteOption]: HeadingModifyOptionMap };
+	};
+} = {
+	noteOptions: [...Object.keys(NoteOption)] as Array<keyof typeof NoteOption>,
+	modifyOptions: [...Object.keys(SimpleModifyOption), ...Object.keys(HeadingModifyOption)] as Array<
+		keyof typeof SimpleModifyOption | keyof typeof HeadingModifyOption
+	>,
+	modifyOptionToTypeMap,
+	functionMap: {
+		[ModifyType.SIMPLE]: {
+			[NoteOption.DAILY]: dailySimpleModifyOptions,
+			[NoteOption.NAMED]: noteSimpleModifyOptions,
+		},
+
+		[ModifyType.HEADING]: {
+			[NoteOption.DAILY]: dailyHeadingModifyOptions,
+			[NoteOption.NAMED]: noteHeadingModifyOptions,
+		},
+	},
+};
